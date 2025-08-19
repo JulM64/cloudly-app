c5524d57e3a1cf2cebc667ac5048c42da0e145c1
JulM64 MVENG <julienmveng6@gmail.com>
2025-08-19 20:01:12 +0000

    creation of the app without code

---
+++ react-app/file-upload-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "fileuploadapp236eb12a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
